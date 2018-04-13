84802ad37accad7cf387850580a3dab8c76cffbb
yetimark <blaine.andrada@cuw.edu>
2018-04-13 14:21:42 -0700

    bma
    Added "destroy" cabability from sword to any object tagged "enemy"

---
+++ Assets/Scripts/sword.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if(hit.gameObject.tag == "enemy")
+        {
+            Destroy(hit.gameObject);
+        }
+    }
+}
