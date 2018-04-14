4b70d58c55f9ac82d0551ab06ae3ecb4ae5c653c
yetimark <blaine.andrada@cuw.edu>
2018-04-13 21:53:08 -0700

    BedTime
    Added Spawning ball points and platforms for the balls to move around the map on

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    void Update()
+    {
+        if (this.transform.position.y < -3f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Time.timeScale = 0f;
+        }
+    }
+    //void Start ()
+    // {
+
+    //}
+
+}
---
+++ Assets/Scripts/ballSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject ball;
+
+        void Start ()
+    {
+        StartCoroutine(startSpawning());
+	}
+    IEnumerator startSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(1f, 4f));
+        Instantiate(this.ball, this.spawnPoints[Random.Range(0, this.spawnPoints.Length)].position, Quaternion.identity);
+        StartCoroutine(startSpawning());
+    }
+}
